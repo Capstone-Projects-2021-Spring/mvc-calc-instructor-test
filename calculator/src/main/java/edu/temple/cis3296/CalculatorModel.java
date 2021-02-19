9157ee16183e4bf7b5d1a1e293b3abd9c59a96a3
Dominic Letarte <36549301+domincl@users.noreply.github.com>
2021-02-19 15:47:01 -0500

    Fix addtion

--- calculator/src/main/java/edu/temple/cis3296/CalculatorModel.java
+++ calculator/src/main/java/edu/temple/cis3296/CalculatorModel.java
@@ -103,7 +103,7 @@ public class CalculatorModel {
     }
 
     public double operationAdd(double lhs, double rhs) {
-        return lhs + lhs;
+        return lhs + rhs;
     }
 
     public double operationSubstr(double lhs, double rhs) {
